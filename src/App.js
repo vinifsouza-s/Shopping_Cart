2a2d8ea0c83cd984c3dd3f90970623d787af3ee0
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-04-30 16:43:32 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [products] = useState ([
+    { 
+      name: 'PS5 Controller',
+      cost: '$40.00',
+      image: 'https://www.paraeles.pt/wp-content/uploads/2020/04/ps5.png'
+    },
+    { 
+      name: 'Xbox Controller',
+      cost: '$35.00',
+      image: 'https://tecnoblog.net/wp-content/uploads/2020/10/xbox-series-x-controle-700x484.jpg'
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Products</h1>
+     <div className='products'>
+       {products.map((product, index) => (
+         <div className='product' key={index}>
+            <h3> {product.name} </h3>
+            <h4> {product.cost} </h4>
+            <img src= {product.image} alt={product.name}/>
+            <br />
+            <button>Add to Cart</button>
+          </div>
+       ))}
+     </div>
     </div>
   );
 }
